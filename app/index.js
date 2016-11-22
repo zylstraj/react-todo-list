49b91899cca3a50c2edaa42adcb365450af648ab
Josh Zylstra <zylstrajosh@gmail.com>
2016-11-22 11:45:41 -0800

    small changes

--- app/index.js
+++ app/index.js
@@ -6,14 +6,17 @@ var TodoComponent = React.createClass({
     return {
       todos: ['study', 'workout', 'meditate', 'sleep']
     }
-  },
+  }
   render: function(){
+
       var todos = this.state.todos;
       todos = todos.map(function(item, index){
         return(
-          <li key={index} className="fun">{item}</li>
+          <li key={index} className="fun">{item}
+          <button className="item-delete" key={index} onClick={this.onDelete}>Delete</button>
+          </li>
           )
-      });
+      }.bind(this));
       return(
         <div>
         <h1>Fun Times</h1>
