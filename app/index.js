8cd10befed01ccb691d597178821f3cd7ad20ffc
Josh Zylstra <zylstrajosh@gmail.com>
2016-11-21 16:22:53 -0800

    learning more about index.js and where everything goes

--- app/index.js
+++ app/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 var TodoComponent = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: ['study', 'workout', 'meditate', 'sleep']
+    }
+  },
   render: function(){
+      var todos = this.state.todos;
+      todos = todos.map(function(item, index){
+        return(
+          <li key={index} className="fun">{item}</li>
+          )
+      });
       return(
-    <h1>Fun Times</h1>
+        <div>
+        <h1>Fun Times</h1>
+        <ul>{todos}</ul>
+        </div>
     )
   }
 })
