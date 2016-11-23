64f916352be47131d39e88536157eb192e2dc162
Josh Zylstra <zylstrajosh@gmail.com>
2016-11-22 18:08:18 -0800

    delete button is working, two components now exist

--- app/index.js
+++ app/index.js
@@ -6,24 +6,45 @@ var TodoComponent = React.createClass({
     return {
       todos: ['study', 'workout', 'meditate', 'sleep']
     }
-  }
+  },
   render: function(){
 
       var todos = this.state.todos;
       todos = todos.map(function(item, index){
         return(
-          <li key={index} className="fun">{item}
-          <button className="item-delete" key={index} onClick={this.onDelete}>Delete</button>
-          </li>
+          <TodoItem item = {item} key ={index} onDelete = {this.onDelete}/>
           )
       }.bind(this));
       return(
-        <div>
+        <div id="todo-list">
         <h1>Fun Times</h1>
         <ul>{todos}</ul>
         </div>
     )
+  },
+  onDelete: function(item){
+    var updatedTodos = this.state.todos.filter(function(val, index){
+      return item !== val;
+    })
+    this.setState({
+      todos: updatedTodos
+    })
   }
 })
 
+var TodoItem = React.createClass({
+  render: function(){
+    return(
+      <li className="fun">
+      <span className="item-name">{this.props.item}</span>
+          <button className="item-delete" onClick={this.handleDelete}>Delete</button>
+          </li>
+
+      )
+  },
+  // Custom Functions
+  handleDelete: function(){
+    this.props.onDelete(this.props.item);
+  }
+})
 ReactDOM.render(<TodoComponent />, document.getElementById('todo'));
